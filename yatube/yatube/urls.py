26d78581c7f45bcdb302054bd36746cacde898bf
Павел Мышкин <pavelmyskin@MacBook-Pro-Pavel.local>
2022-03-01 02:04:29 +0500

    1.03.22

---
+++ yatube/yatube/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('posts.urls', namespace='posts')),
+    path('group/<slug:slug>/', include('posts.urls', namespace='groups')),
+    path('admin/', admin.site.urls),
+]
